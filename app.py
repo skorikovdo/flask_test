589c0708eb96d897db16d7cc6cb2bc7699e5fc30
Denis <skrikov.denis@gmail.com>
2018-08-07 04:20:00 +0300

    corrections api method api/message/list corrections api method api/message/post add config JSON_SORT_KEYS for return jsonify in reverse order(api/message/post) add unittest

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask, request, Response, jsonify, make_response
+from flask import Flask, request, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-app = Flask (__name__)
-app.config.from_object (Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-db = SQLAlchemy (app)
+db = SQLAlchemy(app)
 
 
-@app.route ('/')
-def hello_world ():
+@app.route('/')
+def hello_world():
     return 'Hello World!'
 
 
 @app.route('/api/user/create', methods=['POST'])
-def create_user ():
+def create_user():
     from model import User
     if request.method == 'POST':
         form = request.get_json(force=True)
@@ -29,8 +29,8 @@ def create_user ():
 def user_list():
     from model import User
     if request.method == 'GET':
-        return jsonify([{'id': User.id, 'name': User.name}
-                        for User in User.query.all()])
+        return jsonify([{'id': user.id, 'name': user.name}
+                        for user in User.query.all()])
     return Response(status=404)
 
 
@@ -38,8 +38,9 @@ def user_list():
 def show_user(user_id):
     from model import User
     if request.method == 'GET':
-        return jsonify([{'id': User.id, 'name': User.name}
-                        for User in User.query.filter(User.id == user_id).all()])
+        return jsonify([{'id': user.id, 'name': user.name}
+                        for user in User.query
+                                        .filter(User.id == user_id).all()])
     return Response(status=404)
 
 
@@ -47,17 +48,18 @@ def show_user(user_id):
 def followers_create():
     """
     Follow: User X now follows user Y
-    If User X now follows user Y is True -> Unfollow: User X no longer follows user Y
+    If User X now follows user Y is True ->
+    Unfollow: User X no longer follows user Y
     Выводим
     """
-    from model import User, followers
+    from model import User
     if request.method == 'POST':
         form = request.get_json(force=True)
         user = User.query.filter(User.id == form['follower_id']).one()
         foll = User.query.filter(User.id == form['followed_id']).one()
         if foll in user.followed.all():
             user.followed.remove(foll)
-            db.session.commit ()
+            db.session.commit()
         else:
             user.followed.append(foll)
             db.session.commit()
@@ -67,12 +69,12 @@ def followers_create():
 
 @app.route('/api/followers/list', methods=['GET'])
 def followers_list():
-    from model import User, followers
+    from model import User
     if request.method == 'GET':
         user_to_followers = {}
-        for i in User.query.all():
-            if i.followers.all() != []:
-                user_to_followers[i.name] = [m.name for m in i.followers.all()]
+        for user in User.query.all():
+            if user.followed.all():
+                user_to_followers[user.name] = [user.name for user in user.followed.all()]
         return jsonify(user_to_followers)
     return Response(status=404)
 
@@ -82,10 +84,11 @@ def create_message():
     from model import Message
     if request.method == 'POST':
         form = request.get_json(force=True)
-        message = Message(user_id=form['user_id'], message_text=form['message_text'])
+        message = Message(user_id=form['user_id'],
+                          message_text=form['message_text'])
         db.session.add(message)
         db.session.commit()
-    return Response(status=200)
+    return Response(status=404)
 
 
 @app.route('/api/message/post', methods=['POST'])
@@ -93,43 +96,37 @@ def user_post():
     """
     Post(X,Y): Post message X as user Y
     """
-    from model import User, Message
-    if request.method == 'POST':
-        form = request.get_json(force=True)
-        id={}
-        for i in Message.query.all():
-            print(i.id, i.user.id)
-            if int(form['id']) == i.user.id:
-                for m in Message.query.filter(Message.user_id == form['id']).order_by(Message.date_time.desc()).all():
-                    id[m.id] = {'message_text': m.message_text, 'user_id': m.user.id, 'user': m.user.name}
-                    print(id)
-                return jsonify(id)
-    return Response(status=404)
-
-
-
-
-@app.route('/api/message/timeline', methods=['POST'])
-def timeline():
     from model import Message
     if request.method == 'POST':
         form = request.get_json(force=True)
+        post_id = {}
+        for message in Message.query.all():
+            if int(form['id']) == message.user.id:
+                for message_user in Message.query.filter(Message.user_id == form['id'])\
+                                            .order_by(Message.date_time.desc()).all():
+                    post_id[message_user.id] = dict(message_text=message_user.message_text,
+                                                    user_id=message_user.user.id,
+                                                    user=message_user.user.name)
+                return jsonify(post_id)
+    return Response(status=404)
+
+
+@app.route('/api/message/list', methods=['POST'])
+def message_list():
+    from model import Message, User
+    if request.method == 'POST':
+        form = request.get_json(force=True)
         user = User.query.filter(User.id == form['id']).one()
-        id = {}
-        n = 1
-        id = {}
-        for i in user.followed.all ():
-            message_timeline = db.session.query (User.name, Message.message_text, Message.date_time).filter (
-                User.id == Message.user_id, User.id == i.id).order_by (
-                Message.date_time.desc ()).one ()
-            if message_timeline:
-                id[user.name + str(n)] = {'user_name': message_timeline[0], 'message': message_timeline[1], 'datetime': message_timeline[2]}
-                n += 1
-        return jsonify(id)
+        message_id = {}
+        followed_id_list = [i.id for i in user.followed.all()]
+        for i in db.session.query(Message.date_time, User.name, Message.message_text, User, Message.id).join(User) \
+                .filter(User.id.in_(followed_id_list)).order_by(Message.date_time.desc()).all():
+            message_id[i.id] = dict(message_date_time=i.date_time, message_text=i.message_text, autor_id=i.name)
+        return jsonify(message_id)
     return Response(status=404)
 
 
 if __name__ == '__main__':
     from model import *
-    db.create_all ()
-    app.run ()
+    db.create_all()
+    app.run()
